3b94066817b8ff81aea41010c09065461a0fb3de
Roman <smolyakov_ra@mail.ru>
2024-08-07 20:34:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/main.css';
+import ShopItemClass from './ShopItemClass';
+import ShopModel from './models/ShopModel';
 
 function App() {
+  const item = ShopModel.items
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
